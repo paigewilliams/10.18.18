213e1cae07681a26928856feea0e0a6d8e702c23
Paige Williams and Ralph Perdomo <ralph@>
2018-10-18 11:25:00 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(function() {
+  var gender;
+  var date;
 
+  $("#gender").change(function() {
+    gender = $("#gender").val();
+    $(".date-select").show();
 
+  })
+  $("#date").change(function(){
+    date = $("#date").val();
+    debugger
+  })
+  $("#matchmaker").submit(function(event) {
+
+    event.preventDefault();
+  })
 
 })
