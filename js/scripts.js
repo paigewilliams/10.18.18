714b2df7f080f9e76132cb6ee98f8ff495b3e4e5
Paige Williams and Ralph Perdomo <ralph@>
2018-10-18 10:09:41 -0700

    first animal-chooser commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(function() {
-
+  $("#animal").change(function(){
+    var option = $("#animal").val();
+  
+  if (option === "insects"){
+    console.log("hi");
+  }
+  else if (option === "snakes"){
+    console.log("hey");
+  }
+  else if (option === "turtles"){
+    console.log("hola");
+  }
+  });
 
 
 })
